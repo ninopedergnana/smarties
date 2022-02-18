dff718da175fb819f19bd30d45fb3d92594d0f84
pedernin <pedernin@students.zhaw.ch>
2022-02-18 16:48:39 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,15 @@ var box = null
 
 async function setup() {
     // Set up an ethers contract, representing our deployed Box instance
-    const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    const localAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    const rinkebyAddress = '0x8413F1153504d7D2074D39CbAb379Ca87bf4Cb39';
     const Box = await ethers.getContractFactory('Box');
-    box = await Box.attach(address);
+    box = await Box.attach(rinkebyAddress);
 }
 
 async function sendTransactiontoStore() {
     // Send a transaction to store() a new value in the Box
-    await box.store(23);
+    await box.store(69);
 }
 
 async function fetchAccounts() {
